49561c6e783816db972472a51a4826366ac66050
Önder Görmez <ondergormez@gmail.com>
2022-03-27 14:18:57 +0300

    Update source files

--- Homework1/FastLinkedList.h
+++ Homework1/FastLinkedList.h
@@ -1,1 +1,6 @@
 #pragma once
+
+typedef struct
+{
+
+} FastLinkedList;
--- Homework1/main.c
+++ Homework1/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "FastLinkedList.h"
 
 int main(void)
 {
     printf("Fast Linked List Implementation\n");
+    FastLinkedList *list = NULL; // TODO: malloc and zerois
 
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("Hello: %d\n", i);
+    }
+    bool isUpper = true;
     return 0;
 }
