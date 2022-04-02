e9b16201969ac2e37739f0320afe5ab702e67084
Önder Görmez <ondergormez@gmail.com>
2022-04-03 01:53:48 +0300

    Add convertToString function

--- Homework2/main.c
+++ Homework2/main.c
@@ -1,5 +1,8 @@
 #include "Stack.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int getRequiredBitCountForUnsigned(unsigned long value)
 {
     if (value <= 255)
@@ -26,6 +29,33 @@ int getRequiredBitCountForSigned(signed long value)
         return 24;
 
     return 32;
+}
+
+/* It is responsibility to free the memory caller side  */
+const char* convertToString(unsigned long value, int bitCount)
+{
+    int index = bitCount - 1;
+    char* string = (char*)malloc(sizeof(char) * (bitCount + 1));
+    string[bitCount] = NULL;
+
+    if (string == NULL) {
+        printf("In function convertToString(): Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    while (value != 0) {
+        if (value % 2)
+            string[index--] = '1';
+        else
+            string[index--] = '0';
+
+        value /= 2;
+    }
+
+    while (index >= 0)
+        string[index--] = '0';
+
+    return string;
 }
 
 int main(void)
