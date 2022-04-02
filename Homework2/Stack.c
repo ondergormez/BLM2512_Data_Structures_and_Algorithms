c10636494b8542b0920b13548185cb6ca5820cb2
Önder Görmez <ondergormez@gmail.com>
2022-04-03 01:45:07 +0300

    Add isStackEmpty function

--- Homework2/Stack.c
+++ Homework2/Stack.c
@@ -1,1 +1,13 @@
 #include "Stack.h"
+
+#include <stdio.h>
+
+bool isStackEmpty(Stack_t* stack)
+{
+    if (stack == NULL) {
+        printf("In function isStackEmpty(): Stack pointer null!");
+        return false;
+    }
+
+    return stack->top == 0;
+}
--- Homework2/Stack.h
+++ Homework2/Stack.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stdbool.h>
+
 #define MAX_ELEMENT_COUNT 1000
 
 typedef struct Stack {
     int item[MAX_ELEMENT_COUNT];
     int top;
 } Stack_t;
+
+bool isStackEmpty(Stack_t* stack);
