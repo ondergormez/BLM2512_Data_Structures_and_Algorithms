fe412da8013f4df56100f2faf0d0c18d526ec89e
Önder Görmez <ondergormez@gmail.com>
2022-04-02 22:39:00 +0300

    Add main driver code

---
+++ Homework2/main.c
@@ -0,0 +1,20 @@
+#include "Stack.h"
+
+int main(void)
+{
+    Stack_t stack = { 0 };
+    int value = 0;
+
+    pushToStack(&stack, 10);
+    pushToStack(&stack, 15);
+    pushToStack(&stack, 20);
+    printValuesInStack(&stack);
+
+    popFromStack(&stack, &value);
+    printValuesInStack(&stack);
+
+    peekValueFromStack(&stack, &value);
+    printf("Peek value: %d\n", value);
+
+    return 0;
+}
